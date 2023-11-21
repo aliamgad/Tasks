679dc8c0f5a9f13b57b78d3f92e6631bd759fcf9
aliamgad <129622410+aliamgad@users.noreply.github.com>
2023-11-22 00:33:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void  main (String args [])
+    {
+        float a = Float.valueOf("10.5").floatValue();
+        System.out.println(a);
+        
+
+
+    }
+}
